f10f16cca067883691e7d63cae847b4b035c0e98
mcc.josueAvalos <josue@ucol.mx>
2022-06-10 11:57:43 -0500

    add files

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="footer d-flex flex-wrap justify-content-between align-items-center py-2 my-3 border-top">
+    <div>
+        © Todos los derechos reservados 2022
+    </div>
+    <ul class="nav justify-content-end d-flex">
+        <li>
+            <i class="bi bi-facebook" style="font-size: 2rem; color: cornflowerblue;"></i>
+        </li>
+    </ul>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('include.head')
+    </head>
+    <body class="container-fluid">
+
+    </body>
+    <footer>
+        @include('include.footer')
+    </footer>
+</html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
